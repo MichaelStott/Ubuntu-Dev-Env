8da28965bb154950c96c88ddf30c0ac01a41bcfd
Michael <stottmichael2@gmail.com>
2019-01-20 23:26:33 -0500

    Apply emacs theme

--- editors/emacs.sh
+++ editors/emacs.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
+THEME_DIR=$HOME"/.emacs.d/themes/"
+
+THEME="melancholy-theme.el"
+THEME_GIT_RAW="https://raw.githubusercontent.com/techquila/melancholy-theme/master/"
+
 # Get emacs
 apt-get install emacs
 
 # Apply nice theme :)
+mkdir $THEME_DIR
+if [ ! -f $THEME_DIR$THEME ]; then
+    wget $THEME_GIT_RAW$THEME -P $THEME_DIR
+    echo "(load-file \"$THEME_DIR$THEME\")" >> "$HOME/.emacs"
+fi
 
 echo "Finsished installing emacs!"
